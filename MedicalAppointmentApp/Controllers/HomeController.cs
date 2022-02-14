918e5d4b35d838ae00ab528999b7d2afb77a2664
titas251 <krtitas1@gmail.com>
2022-02-14 19:18:58 +0200

    Add project files.

---
+++ MedicalAppointmentApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using MedicalAppointmentApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicalAppointmentApp.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("Privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet("Error")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MedicalAppointmentApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MedicalAppointmentApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
