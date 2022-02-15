4eca44aa30cc5b5aa1b07d86b07b698c87322881
titas251 <krtitas1@gmail.com>
2022-02-15 14:49:21 +0200

    Added initialize db functionality

---
+++ MedicalAppointmentApp/Data/DbInitializer.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicalAppointmentApp.Data
+{
+    public class DbInitializer
+    {
+        public static void SeedData(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            SeedRoles(roleManager);
+            SeedUsersAsync(userManager);
+        }
+
+        public static void SeedUsersAsync(UserManager<IdentityUser> userManager)
+        {
+            if (userManager.FindByNameAsync("admin").Result == null)
+            {
+                var user = new IdentityUser
+                {
+                    Email = "admin@gmail.com",
+                    NormalizedEmail = "ADMIN@GMAIL.COM",
+                    UserName = "admin",
+                    NormalizedUserName = "ADMIN",
+                    PhoneNumber = "+370622222",
+                    EmailConfirmed = true,
+                    PhoneNumberConfirmed = true,
+                    SecurityStamp = Guid.NewGuid().ToString("D")
+                };
+
+                var password = new PasswordHasher<IdentityUser>();
+                user.PasswordHash = password.HashPassword(user, "123");
+
+                var result = userManager.CreateAsync(user).Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Admin").Wait();
+                }
+            }
+        }
+
+        public static void SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            if (!roleManager.RoleExistsAsync("Admin").Result)
+            {
+                IdentityRole role = new IdentityRole()
+                {
+                    Name = "Admin"
+                };
+                roleManager.CreateAsync(role);
+            }
+        }
+    }
+}
