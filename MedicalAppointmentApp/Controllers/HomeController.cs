ef77227177fa35c31c0f6c7dbc0f80916abc7799
unknown <g.minkstimas@gmail.com>
2022-02-16 21:32:58 +0200

    Added admin tab to display and delete users

--- MedicalAppointmentApp/Controllers/HomeController.cs
+++ MedicalAppointmentApp/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MedicalAppointmentApp.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -14,10 +15,12 @@ namespace MedicalAppointmentApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<IdentityUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -33,11 +36,40 @@ namespace MedicalAppointmentApp.Controllers
             return View();
         }
 
+        [HttpGet("AdminDataAccess")]
+        public IActionResult AdminDataAccess()
+        {
+            return View(new AdminDataAccessModel(_userManager));
+        }
+
         [HttpGet("error")]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityUser user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await _userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                    Errors(result);
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+            return View("AdminDataAccess");
+        }
+
+        private void Errors(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("", error.Description);
         }
     }
 }
---
+++ MedicalAppointmentApp/Models/AdminDataAccessModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicalAppointmentApp.Models
+{
+    public class AdminDataAccessModel
+    {
+        public readonly UserManager<IdentityUser> userManager;
+
+        public AdminDataAccessModel(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+    }
+}
