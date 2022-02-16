72a9698700c3dcd2c2942e96e632b201ad5b0605
titas251 <krtitas1@gmail.com>
2022-02-16 12:19:29 +0200

    Added google authenticator

--- MedicalAppointmentApp/Startup.cs
+++ MedicalAppointmentApp/Startup.cs
@@ -91,6 +91,16 @@ namespace MedicalAppointmentApp
                     }
                 });
             });
+
+            services.AddAuthentication()
+            .AddGoogle(options =>
+            {
+                IConfigurationSection googleAuthNSection =
+                    Configuration.GetSection("Authentication:Google");
+
+                options.ClientId = googleAuthNSection["ClientId"];
+                options.ClientSecret = googleAuthNSection["ClientSecret"];
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
