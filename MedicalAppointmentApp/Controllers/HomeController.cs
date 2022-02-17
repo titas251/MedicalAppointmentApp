051120737d5ccbf300c2502cda943c700d230eaa
titas251 <krtitas1@gmail.com>
2022-02-17 11:15:40 +0200

    Moved user delete and get to UserController

--- MedicalAppointmentApp/Controllers/HomeController.cs
+++ MedicalAppointmentApp/Controllers/HomeController.cs
@@ -14,12 +14,10 @@ namespace MedicalAppointmentApp.Controllers
     [Route("")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
         private readonly UserManager<IdentityUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger, UserManager<IdentityUser> userManager)
+        public HomeController(UserManager<IdentityUser> userManager)
         {
-            _logger = logger;
             _userManager = userManager;
         }
 
@@ -36,40 +34,11 @@ namespace MedicalAppointmentApp.Controllers
             return View();
         }
 
-        [HttpGet("AdminDataAccess")]
-        public IActionResult AdminDataAccess()
-        {
-            return View(new AdminDataAccessModel(_userManager));
-        }
-
         [HttpGet("error")]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Delete(string id)
-        {
-            IdentityUser user = await _userManager.FindByIdAsync(id);
-            if (user != null)
-            {
-                IdentityResult result = await _userManager.DeleteAsync(user);
-                if (result.Succeeded)
-                    return RedirectToAction("Index");
-                else
-                    Errors(result);
-            }
-            else
-                ModelState.AddModelError("", "User Not Found");
-            return View("AdminDataAccess");
-        }
-
-        private void Errors(IdentityResult result)
-        {
-            foreach (IdentityError error in result.Errors)
-                ModelState.AddModelError("", error.Description);
         }
     }
 }
---
+++ MedicalAppointmentApp/Models/UserWithRoleModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicalAppointmentApp.Models
+{
+    public class UserWithRoleModel
+    {
+        public IdentityUser User { get; set; }
+        public List<string> Roles { get; set; }
+    }
+}
