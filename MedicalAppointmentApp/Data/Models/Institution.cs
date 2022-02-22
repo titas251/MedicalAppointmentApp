47aff38de9a850bf5486a047f585f3f3ba6a71af
titas251 <krtitas1@gmail.com>
2022-02-22 11:40:36 +0200

    Added Doctor, Institution, MedicalSpeciality entities and added migrations

---
+++ MedicalAppointmentApp/Data/Models/Institution.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MedicalAppointmentApp.Data.Models
+{
+    public class Institution
+    {
+        [Key]
+        public int InstitutionId { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public virtual List<InstitutionDoctor> Doctors { get; set; }
+
+    }
+}
