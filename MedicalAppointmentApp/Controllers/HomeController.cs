7fa15c01fcbb59d0aa289f060eacd85684cec40c
gintarasmi <g.minkstimas@gmail.com>
2022-03-14 15:34:01 +0200

    Added paging to search page.

--- MedicalAppointmentApp/Controllers/HomeController.cs
+++ MedicalAppointmentApp/Controllers/HomeController.cs
@@ -32,11 +32,25 @@ namespace MedicalAppointmentApp.Controllers
         }
 
         [HttpGet("search")]
-        public async Task<IActionResult> SearchView([FromQuery(Name = "q")] string q, int? page, int? pageSize)
+        public async Task<IActionResult> SearchView(
+            [FromQuery(Name = "q")] string q,
+            [FromQuery(Name = "currentFilter")] string currentFilter,
+            [FromQuery(Name = "pageNumber")] int? pageNumber,
+            [FromQuery(Name = "pageSize")] int? pageSize)
         {
-            int numOfAppointmentsToGet = 10;
+            int numOfAppointmentsToGet = 3;
 
-            var doctorsViewModel = await _mediator.Send(new GetDoctorsByQuery.Query(q ?? "", numOfAppointmentsToGet, page ?? 1, pageSize ?? 10));
+            if (q != null) pageNumber = 1;
+                else q = currentFilter;
+
+            ViewBag.CurrentFilter = q;
+            ViewBag.PageNumber = pageNumber ?? 1;
+            ViewBag.PageSize = pageSize ?? 10;
+
+            int doctorCount = await _mediator.Send(new GetDoctorCountByQuery.Query(q ?? ""));
+            ViewBag.HasNextPage = Math.Ceiling((double)doctorCount / (double)(pageSize ?? 10)) == (pageNumber ?? 1);
+
+            var doctorsViewModel = await _mediator.Send(new GetDoctorsByQuery.Query(q ?? "", numOfAppointmentsToGet, pageNumber ?? 1, pageSize ?? 10));
             return View("Search", doctorsViewModel);
         }
 
---
+++ MedicalAppointmentApp/Mediator/Queries/GetDoctorCountByQuery.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using MediatR;
+using MedicalAppointmentApp.Data;
+using MedicalAppointmentApp.Data.Models;
+using MedicalAppointmentApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MedicalAppointmentApp.Queries
+{
+    public static class GetDoctorCountByQuery
+    {
+        public class Query : IRequest<int>
+        {
+            public string StringQuery { get; }
+
+            public Query(string stringQuery)
+            {
+                StringQuery = stringQuery;
+            }
+        }
+
+        public class Handler : IRequestHandler<Query, int>
+        {
+            private readonly ApplicationDbContext _context;
+
+            public Handler(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<int> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var doctorCount = await _context.Doctors
+                    .Include(doctor => doctor.MedicalSpeciality)
+                    .Include(doctor => doctor.Appointments)
+                    .Include(doctor => doctor.Schedules)
+                        .ThenInclude(schedule => schedule.ScheduleDetails)
+                    .Include(doctor => doctor.Schedules)
+                        .ThenInclude(schedule => schedule.Institution)
+                    .Where(doctor => doctor.FirstName.Contains(request.StringQuery)
+                    || doctor.LastName.Contains(request.StringQuery)
+                    || (doctor.FirstName + " " + doctor.LastName).Contains(request.StringQuery)
+                    || doctor.MedicalSpeciality.Name.Contains(request.StringQuery)
+                    || doctor.Schedules.Any(c => c.Institution.Name.Contains(request.StringQuery)))
+                    .CountAsync();
+
+
+                return doctorCount;
+            }
+        }
+    }
+}
