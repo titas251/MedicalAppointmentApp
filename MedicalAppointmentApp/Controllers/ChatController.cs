86d0b122bd03dcdc9947235a3c6df8cf8bf4fd6b
titas251 <krtitas1@gmail.com>
2022-03-22 09:55:25 +0200

    Separated chat window and added user email to show in chat

---
+++ MedicalAppointmentApp/Controllers/ChatController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalAppointmentApp.Controllers
+{
+    [Route("[controller]")]
+    public class ChatController : Controller
+    {
+        public IActionResult GetChatView()
+        {
+            return View("Views/Chat/Chat.cshtml");
+        }
+    }
+}
