488d447acd9e45bb36d0f2c35c2c28bfadf8fdda
titas251 <krtitas1@gmail.com>
2022-03-29 12:03:53 +0300

    Removed new

--- Database/Repositories/MedicalSpecialityRepository.cs
+++ Database/Repositories/MedicalSpecialityRepository.cs
@@ -11,10 +11,8 @@ namespace DAL.Repositories
 {
     public class MedicalSpecialityRepository : Repository<MedicalSpeciality>, IMedicalSpecialityRepository
     {
-        private new readonly ApplicationDbContext _context;
         public MedicalSpecialityRepository(ApplicationDbContext context) : base(context)
         {
-           _context = context;
         }
 
         public async Task DeleteAsync(int specialityId)
